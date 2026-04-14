d7e9fad42a80a5d9b0da1a4256c5b8e265506885
vishalsaraswatcs23-sudo <vishal.saraswat_cs23@gla.ac.in>
2026-04-14 15:30:22 +0530

    Add authentication-authorization project content

---
+++ todo/model/todomodel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const todoSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    status:{
+        type:String,
+        enum:["pending","completed"],
+        default:"pending"
+    }
+
+
+});
+
+const Todo=mongoose.model("Todo",todoSchema);
+
+export default Todo;
